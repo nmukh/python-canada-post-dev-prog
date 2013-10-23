667f0bc1f7bf13756984d1a79e41d841462dbbdf
marconius <marco@cuthemustard.com>
2013-10-23 11:03:34 -0400

    skeletons for non-cotract shipping and working test for create_shipment

--- tests/test_contract_shipping.py
+++ tests/test_contract_shipping.py
@@ -2,18 +2,46 @@
 
 import unittest
 
+from canada_post.service import contract_shipping, Service
+from canada_post.util import parcel, address
+from . import api_details
 
-class TestContractShipping(unittest.TestCase)
+class TestContractShipping(unittest.TestCase):
     """
     Tests the functions specific to contract shipping as detailed on Canada 
     Posts's Contract Shipping documentation
     """
     
     def setUp(self):
-        pass
-      
+        self.parcel = parcel.Parcel(
+            weight=1, length=20, width=20, height=10
+        )
+        self.origin = address.Origin(
+            postal_code="H3Z2Y7",
+            name="John Jones",
+            company="Company Co.",
+            phone="5145555555",
+            address="10-123 1/2 Main St. SE",
+            city="Montreal",
+            province="Quebec"
+        )
+        self.destination = address.Destination(
+            postal_code="V8X3X4",
+            name="Jane Jones",
+            phone="6145555555",
+            address="1425 James St.",
+            city="Victoria",
+            province="British Columbia",
+            country_code="CA"
+        )
+        self.service = Service(data={"name": "Expedited Parcel", 
+                                     "code": "DOM.EP"})
+    
+    
     def test_create_shipment(self):
-        pass
+        cp_req = contract_shipping.CreateShipment(api_details.AUTH)
+        cp_req(self.parcel, self.origin, self.destination, self.service,
+               "fake_group_id")
     
     def test_get_shipment(self):
         pass
---
+++ tests/test_non_contract_shipping.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+
+import unittest
+
+class TestNonContractShipping(unittest.TestCase):
+    """
+    Tests the Non-Contract Shipping use case  as detailed on Canada 
+    Posts's Non-Contract Shipping documentation
+    """
+    
+    def setUp(self):
+        pass
+    
+    def test_create_nc_shipment(self):
+        """
+        The typical route used for shipping without a Canada Post contract:
+        
+        * Create Non-Contract Shipment
+        
+        * Get Artifact
+        """
+        pass
+    
+    def test_get_nc_shipment_receipt(self):
+        """
+        Test the Get Non-Contract Shipment receipt
+        """
+        pass
+    
+    def test_get_nc_shipment_details(self):
+        """
+        Tests Get Non-Contract Shipment Details
+        """
+    
+    def test_get_nc_shipments(self):
+        """
+        Tests Get Non-Contract Shipments
+        """
+        pass
+    
+    def test_get_nc_shipment(self):
+        """
+        Get Non-Contract Shipment
+        """
+        pass
